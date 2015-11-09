22363ad6509513b05137780dc8edc0ef88f33875
pengwei <pengwei1024@gmail.com>
2015-11-09 16:36:55 +0800

    add demo

--- app/src/main/java/com/apkfuns/simplerecycleradapterdemo/MainActivity.java
+++ app/src/main/java/com/apkfuns/simplerecycleradapterdemo/MainActivity.java
@@ -1,41 +1,15 @@
 package com.apkfuns.simplerecycleradapterdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.preference.PreferenceActivity;
 
-import com.apkfuns.simplerecycleradapter.SimpleRecyclerAdapter;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerView;
-
+/**
+ * Created by pengwei08 on 15/11/9.
+ */
+public class MainActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyRecyclerAdapter(new String[]{"1", "2", "3", "4"}));
+        addPreferencesFromResource(R.xml.main);
     }
-
-    class MyRecyclerAdapter extends SimpleRecyclerAdapter<String> {
-
-        public MyRecyclerAdapter(String[] array) {
-            super(array);
-        }
-
-        @Override
-        public int getLayoutRes() {
-            return R.layout.view_text;
-        }
-
-        @Override
-        public void onBindView(RVHolder holder, int position, String s) {
-            holder.setTextView(R.id.text, s);
-        }
-    }
-
 }
---
+++ app/src/main/java/com/apkfuns/simplerecycleradapterdemo/MultipleListActivity.java
@@ -0,0 +1,10 @@
+package com.apkfuns.simplerecycleradapterdemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pengwei08 on 15/11/9.
+ */
+public class MultipleListActivity extends AppCompatActivity {
+    
+}
